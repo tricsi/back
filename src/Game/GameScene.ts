a6bdd3c7a30ca625abbd6f8c87fd4088498d4412
Csaba Csecskedi <csaba.csecskedi@possible.com>
2019-09-11 22:16:07 +0200

    maps

--- src/Game/GameScene.ts
+++ src/Game/GameScene.ts
@@ -14,7 +14,6 @@ import sfx from "../sfx";
 
 export default class GameScene extends GameObject {
 
-    hero = new Hero(config.hero);
     cam = new Camera(this.hero, config.cam, this.map.bottom);
     hud = new Hud(this.hero, this.cam);
     aim = new Vec();
@@ -24,7 +23,7 @@ export default class GameScene extends GameObject {
     worms: ObjectPool = new ObjectPool(() => new EnemyWorm(this.hero, config.worm));
     explos: ObjectPool = new ObjectPool(() => new Explosion());
 
-    constructor(public map: TileMap) {
+    constructor(public hero: Hero, public map: TileMap) {
         super();
         this.hero.pos.set(96, 112);
         this.map.createNav(this.hero.box.center);
