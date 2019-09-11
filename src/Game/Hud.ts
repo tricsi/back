50fedd4c122c4388738372068ad57c323ea7d078
Csaba Csecskedi <csaba.csecskedi@possible.com>
2019-09-11 21:32:34 +0200

    maps

--- src/Game/Hud.ts
+++ src/Game/Hud.ts
@@ -14,7 +14,7 @@ export enum GameStatus {
 export default class Hud extends GameObject {
 
     satus = GameStatus.start;
-    text = new Txt(new Vec(16));
+    text = new Txt(new Vec(3));
     message = new Txt(new Vec(32, 120));
     messages: string[] = ["Click to Start", "", "Game Over", "Level Complete"];
 
@@ -29,8 +29,10 @@ export default class Hud extends GameObject {
         const box1 = this.cam.box;
         const box2 = this.message.box;
         const hero = this.hero;
+        const hp = new String(hero.hp + 1000).substr(1);
+        const ammo = new String(hero.grenades.ammo + 100).substr(1);
         const score = new String(hero.score + 1000000).substr(1);
-        this.text.text = `SQUAD ${hero.lives}  GRENADE ${hero.grenades.ammo}  SCORE ${score}`;
+        this.text.text = `MEN ${hero.lives}  HP ${hp}  AMMO ${ammo}  SCORE ${score}`;
         this.message.pos.set((box1.width - box2.width) / 2, (box1.height - box2.height) / 2);
         this.message.text = this.messages[this.satus];
     }
